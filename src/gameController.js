8d8cc5e71cd2662b095d9d9c8fa966fb0684d6c6
Jacob O'Donnell <jacobodonnell@gmail.com>
2019-07-08 20:56:16 -0400

    initial commit

---
+++ src/gameController.js
@@ -0,0 +1,24 @@
+class GameController {
+  constructor() {
+    this.x = 20;
+  }
+
+  update() {
+    this.x = Math.floor(Math.random() * canvas.width);
+  }
+
+  draw() {
+    this.clearScreen();
+    this.drawText();
+  }
+
+  drawText() {
+    ctx.fillStyle = 'red';
+    ctx.font = '48px serif';
+    ctx.fillText('Hello world', this.x, canvas.height / 2);
+  }
+
+  clearScreen() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+  }
+}
