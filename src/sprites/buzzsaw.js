f44f8e3b3adcb23f740a053d55a2893616dac482
markodarko <markmcd26@gmail.com>
2019-07-24 23:06:40 -0400

    buzzsaw
    made buzzsaw out of shapes instead of pixels

--- src/sprites/buzzsaw.js
+++ src/sprites/buzzsaw.js
@@ -45,7 +45,14 @@ class BuzzSaw extends Sprite {
     ctx.save();
     ctx.translate(this.x + this.width() / 2, this.y + this.height() / 2);
     ctx.rotate((this.angle * Math.PI) / 180);
-    super.draw(this.pallet, -this.w / 2, -this.w / 2);
+    //super.draw(this.pallet, -this.w / 2, -this.w / 2);
+    ctx.fillStyle = 'white'
+    ctx.fillRect(-this.width()/2,-this.width()/2,this.width(),this.height())
+    ctx.rotate((45 * Math.PI) / 180);
+    ctx.fillStyle = this.pallet[1]
+    ctx.fillRect(-this.width()/2,-this.width()/2,this.width(),this.height())
+    ctx.fillStyle = this.pallet[0]
+    ctx.fillRect(-7,-7,14,14)
     ctx.restore();
   }
 }
