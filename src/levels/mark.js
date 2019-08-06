86198f28d98b0eb16ddc7ec684bc877c9413e64d
markodarko <markmcd26@gmail.com>
2019-08-06 18:56:49 -0400

    mark's first level

--- src/levels/mark.js
+++ src/levels/mark.js
@@ -1,56 +1,32 @@
 class LevelMark {
-  constructor(onscreenSprites) {
-    onscreenSprites.startX = 270;
-    onscreenSprites.startY = 450;
-    onscreenSprites.player = new Player(onscreenSprites.startX, onscreenSprites.startY, images.img.hero);
-
-    onscreenSprites.NextLevel = LevelJacob;
-    onscreenSprites.goal = new Swapper(...tileToWorld(18, 6), images.pallet.yellow);
-    onscreenSprites.walls = this.createWalls();
+  constructor(oss) {
+    oss.NextLevel = LevelJacob;
+    oss.goal = new Swapper(...tileToWorld(18, 6), images.pallet.yellow);
+    for (let row=0; row<15; row++){
+	for (let col=0; col<20; col++){
+	  var index = row*20+col
+	  this.build(oss,index,col,row) 
+	}
+    }
   }
+  build(oss,index,x,y){
+    x=x * 8 * PIXEL_SIZE;
+    y=y * 8 * PIXEL_SIZE;
+    switch(LevelMark.bluePrince[index]){
+	case 0: break;
+	case 'player':
+		oss.startX = x;
+		oss.startY = y;		
+		oss.player = new Player(x, y, images.img.hero);
+	break;
+	case 'wall':
+		oss.walls.push(new Block(x,y, images.img.brick));
+	break;
+	case 'break':
+		oss.breakwalls.push(new BreakerBlock(x,y));
+	break;
 
-  numWallsToFillBottom() {
-    const wallsWidth = 8 * PIXEL_SIZE;
-    return canvas.width / wallsWidth;
+    }
   }
-
-  createWalls() {
-    const walls = [];
-
-    const bottomWalls = _.compact(
-      ArrayHelpers.range(this.numWallsToFillBottom()).map(x => {
-        if (x === 10 || x === 11) return null;
-        return new Block(...tileToWorld(x, MAX_Y_GRID_SIZE - 1), images.img.brick);
-      })
-    );
-
-    const wallsWidth = 8 * PIXEL_SIZE;
-    const numWallsToFillSide = canvas.height / wallsWidth;
-    const leftWalls = ArrayHelpers.range(numWallsToFillSide - 1).map(y => {
-      return new Block(...tileToWorld(0, y), images.img.brick);
-    });
-
-    const rightWalls = ArrayHelpers.range(numWallsToFillSide - 1).map(y => {
-      return new Block(...tileToWorld(MAX_X_GRID_SIZE, y), images.img.brick);
-    });
-
-    const topWalls = ArrayHelpers.range(this.numWallsToFillBottom()).map(x => {
-      return new Block(...tileToWorld(x, 0), images.img.brick);
-    });
-
-    walls.push(
-      ...ArrayHelpers.range(8).map(x => {
-        return new Block(...tileToWorld(x + 3, 6), images.img.brick);
-      })
-    );
-
-    walls.push(...bottomWalls);
-    walls.push(...leftWalls);
-    walls.push(...rightWalls);
-    walls.push(...topWalls);
-    walls.push(new Block(...tileToWorld(18, 8), images.img.brick));
-    walls.push(new Block(...tileToWorld(17, 8), images.img.brick));
-    walls.push(new Block(...tileToWorld(14, 8), images.img.brick));
-    return walls;
-  }
+  static bluePrince = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall","wall",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall","wall",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall","wall",0,"player",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall","wall",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall","wall","wall","wall","wall",0,0,"break",0,0,0,"break",0,0,0,"break",0,"wall","wall","wall","wall",0,0,0,"wall",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"wall","break",0,0,0,"break",0,0,0,"break",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 }
